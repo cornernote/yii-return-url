f07e81de69b29084738c888042de3b1c794429af
cornernote <cornernote@gmail.com>
2015-04-21 13:03:16 +0930

    check for scalar before assigning url

--- return-url/components/EReturnUrl.php
+++ return-url/components/EReturnUrl.php
@@ -107,8 +107,8 @@ class EReturnUrl extends CApplicationComponent
     private function getUrlFromSubmitFields()
     {
         $requestKey = $this->requestKey;
-        $url = isset($_GET[$requestKey]) ? $_GET[$requestKey] : (isset($_POST[$requestKey]) ? $_POST[$requestKey] : false);
-        return isset($_GET[$requestKey]) ? $this->urlDecode($url) : $url;
+        $url = isset($_GET[$requestKey]) && is_scalar($_GET[$requestKey]) ? $_GET[$requestKey] : (isset($_POST[$requestKey]) && is_scalar($_POST[$requestKey]) ? $_POST[$requestKey] : false);
+        return isset($_GET[$requestKey]) && is_scalar($_GET[$requestKey]) ? $this->urlDecode($url) : $url;
     }
 
     /**
